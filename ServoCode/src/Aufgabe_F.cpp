310abb242a742a9b055c66b8092b95e9edcd8abf
Nora Hüppi <nehueppi@gmail.com>
2024-11-21 10:15:12 +0100

    changed  name of the buttons

--- ServoCode/src/Aufgabe_F.cpp
+++ ServoCode/src/Aufgabe_F.cpp
@@ -3,31 +3,31 @@
 
 #define SERVO5PIN 12 //Orange
 
-#define BUTTONPIN0 15
-#define BUTTONPIN1 16
-#define BUTTONPIN2 17
-#define BUTTONPIN3 18
+#define BUTTONMOVE180 15
+#define BUTTONMOVE0 16
+#define BUTTONSAVE 17
+#define BUTTONSTART 18
 
 Servo servo5;
 
 int minUs = 1000;
 int maxUs = 2000;
 
-int Button0val = 0;
-int Button1val = 0;
-int Button2val = 0;
-int Button3val = 0;
+int ButtonMove180val = 0;
+int ButtonMove0val = 0;
+int ButtonSaveval = 0;
+int ButtonStartval = 0;
 
-int Button0val_last = 0;
-int Button1val_last = 0;
-int Button2val_last = 0;
-int Button3val_last = 0;
+int ButtonMove180val_last = 0;
+int ButtonMove0val_last = 0;
+int ButtonSaveval_last = 0;
+int ButtonStartval_last = 0;
 
 int counter = 0;
 
 int Delay = 100;
 
-int save[10];
+int Save[10];
 int savespace = 0;
 
 int pos = 0;      // position in degrees
@@ -35,69 +35,53 @@ ESP32PWM pwm;
 
 
 void setup() {
-  // put your setup code here, to run once:
-  // Allow allocation of all timers
-	ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
+    // put your setup code here, to run once:
+    // Allow allocation of all timers
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+    ESP32PWM::allocateTimer(2);
+    ESP32PWM::allocateTimer(3);
 
-  Serial.begin(9600);
-  
-  
-  pinMode(BUTTONPIN0, INPUT_PULLUP);
-  pinMode(BUTTONPIN1, INPUT_PULLUP);
-  pinMode(BUTTONPIN2, INPUT_PULLUP);
-  pinMode(BUTTONPIN3, INPUT_PULLUP);
+    Serial.begin(9600);
 
-  pinMode(SERVO1PIN, OUTPUT);
-  pinMode(SERVO2PIN, OUTPUT);
-  pinMode(SERVO3PIN, OUTPUT);
-  pinMode(SERVO4PIN, OUTPUT);
-  pinMode(SERVO5PIN, OUTPUT);
+    pinMode(BUTTONMOVE180, INPUT_PULLUP);
+    pinMode(BUTTONMOVE0, INPUT_PULLUP);
+    pinMode(BUTTONSAVE, INPUT_PULLUP);
+    pinMode(BUTTONSTART, INPUT_PULLUP);
 
-  pinMode(LEDPin_Red, OUTPUT);
-  pinMode(LEDPin_Blue, OUTPUT);
-  pinMode(LEDPin_Green, OUTPUT);
+    pinMode(SERVO5PIN, OUTPUT);
 
-  servo1.setPeriodHertz(50);
-  servo2.setPeriodHertz(50);
-  servo3.setPeriodHertz(50);
-  servo4.setPeriodHertz(50);
-  servo5.setPeriodHertz(50);      // Standard 50hz servo
+    servo5.setPeriodHertz(50);      // Standard 50hz servo
 
-  servo1.attach(SERVO1PIN, minUs, maxUs);
-  servo2.attach(SERVO2PIN, minUs, maxUs);
-  servo3.attach(SERVO3PIN, minUs, maxUs);
-  servo4.attach(SERVO4PIN, minUs, maxUs);
-  servo5.attach(SERVO5PIN, minUs, maxUs);
+    servo5.attach(SERVO5PIN, minUs, maxUs);
 }
 
 //Aufgabe F)
 void loop(){
-    Button0val = digitalRead(BUTTONPIN0);
-    Button1val = digitalRead(BUTTONPIN1);
-    Button2val = digitalRead(BUTTONPIN2);
-    Button3val = digitalRead(BUTTONPIN3);
+    ButtonMove180val = digitalRead(BUTTONMOVE180);
+    ButtonMove0val = digitalRead(BUTTONMOVE0);
+    ButtonSaveval = digitalRead(BUTTONSAVE);
+    ButtonStartval = digitalRead(BUTTONSTART);
 
-    if(Button0val == LOW) {
-        for(pos; pos >= 180; pos++){
-        servo1.write(pos);
-        counter++;
-        delay(Delay);
-        if(counter >= 10){
-            counter = 0;
-            if(Delay >= 10){
-            Delay -= 5;
+    if(ButtonMove180val == LOW) {
+        for(pos; pos <= 180; pos++){
+            servo1.write(pos);
+            counter++;
+            delay(Delay);
+            if(counter >= 10){
+                counter = 0;
+                if(Delay >= 10){
+                Delay -= 5;
+                }
             }
-        }
         }
     }
     else{
         Delay = 100;
     }
-    if(Button1val <= LOW) {
-        for(pos; pos == 0; pos--){
+    
+    if(ButtonMove0val == LOW) {
+        for(pos; pos >= 0; pos--){
             servo1.write(pos);
             delay(100);
             counter++;
@@ -114,21 +98,21 @@ void loop(){
         Delay = 100;
     }
 
-    if((Button2val != Button2val_last) && (Button2val == LOW)){
-        save[savespace] = pos;
+    if((ButtonSaveval != ButtonSaveval_last) && (ButtonSaveval == LOW)){
+        Save[savespace] = pos;
         savespace++;
         if(savespace <= 9){
-        
+            savespace = 0;
         }
     }
 
-    if((Button3val != Button3val_last) && (Button3val == LOW)){
+    if((ButtonStartval != ButtonStartval_last) && (ButtonStartval == LOW)){
         for(int i = 0; i <= 9; i++){
             servo1.write(save[i]);
             delay(500);
         }
     }
 
-    Button2val_last = Button2val;
-    Button3val_last = Button3val;
+    ButtonSaveval_last = ButtonSaveval;
+    ButtonStartval_last = ButtonStartval;
 }
