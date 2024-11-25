f59d04052939226fcd24872442507ce9a59a461b
norahueppi <nehueppi@gmail.com>
2024-11-25 13:23:59 +0100

    added a header

--- ServoCode/Aufgabe_G/src/Aufgabe_G.cpp
+++ ServoCode/Aufgabe_G/src/Aufgabe_G.cpp
@@ -1,3 +1,33 @@
+/* Aufgabe_F.cpp
+ by Nora Hüppi
+ 
+ If a flank is detected from btn0val than the servos rotate  to a position, pick up a pen and move 
+ to another point and let the pen go.
+ Is a flank from btn1val detected than the pen will be picked up again and put it on the first poitn.
+
+ Connection of the Servos:
+ Servo Datapin (Orange/White) -> Pin12 /Pin11 /Pin10 /Pin9 /Pin8
+ Servo Vcc (Red) -> 5V
+ Servo GND (Brown) -> GND
+ 
+ Connection of Button0:
+ 3.3V -> PIN15
+ 
+ Connection of Button1:
+ 3.3V -> PIN16
+
+ Connection of BUTTONSAVE:
+ 3.3V -> PIN17
+
+ Connection of BUTTONSTART:
+ 3.3V -> PIN18
+
+ created 28 Oktober 2024
+ by Nora Hüppi
+ edited 21 November 2024
+ by Nora Hüppi
+*/
+
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
@@ -19,9 +49,11 @@
 #define statebtn1   1
 #define statebtn2   2
 
-#define servoopen   3
-#define servoclose  4
-#define servomove   5
+#define servoopen       3
+#define servoclose      4
+#define servomovestart  5
+#define servomove       6
+#define servomoveend    7
 
 Servo servo1;
 Servo servo2;
@@ -35,9 +67,6 @@ int maxUs = 2000;
 
 int Button0val = 0;
 int Button1val = 0;
-int Button2val = 0;
-int Button3val = 0;
-
 
 int Button0val_last = 0;
 int Button1val_last = 0;
@@ -50,6 +79,8 @@ int LEDPin_Red = 5;
 
 int stateservo = servostart;
 int statebtn = statebegin;
+
+int statesave = 0;
 
 int counter = 0;
 
@@ -126,7 +157,7 @@ void loop() {
             }
             if((Button1val != Button1val_last) && (Button1val == LOW)){
                 statebtn = statebtn2;
-                stateservo = servoopen;
+                stateservo = servomovestart;
             }
 
             Button0val_last = Button0val;
@@ -140,19 +171,38 @@ void loop() {
             digitalWrite(LEDPin_Green, LOW);
 
             switch(stateservo){
-                case servoopen:
+                case servomovestart:
                     servo1.write(50);
+                    delay(15);
                     servo2.write(140);
+                    delay(15);
                     servo3.write(20);
+                    delay(15);
                     servo4.write(100);
+                    delay(15);
+
+                    statesave = 1;
+
+                    stateservo = servoopen;
+
+                    break;
+
+                case servoopen:
                     servo5.write(180);
+                    delay(15);
 
-                    counter ++;
-                    if(counter >= 25){
-                        stateservo = servoclose;
-                        counter = 0;
+                    if(statesave == 1){
+                        statesave = 0;
+                        counter ++;
+                        if(counter >= 25){
+                            stateservo = servoclose;
+                            counter = 0;
+                        } 
                     }
-
+                    else{
+                        statebtn = statebegin;
+                    }
+                    
                     break;
 
                 case servoclose:
@@ -160,7 +210,7 @@ void loop() {
                     if(SERVO5PIN >= 0){
                         counter ++;
                         if(counter >= 10){
-                            stateservo = servomove;
+                            stateservo = servomoveend;
                             counter = 0;
                         }
                     }
@@ -168,6 +218,116 @@ void loop() {
                     break;
 
                 case servomove:
+                    servo2.write(90);
+                    delay(15);
+                    servo3.write(90);
+                    delay(15);
+                    servo4.write(90);
+                    delay(15);
+                    servo1.write(160);
+                    delay(15);
+                    
+                    stateservo = servomoveend;
+
+                    break;
+
+                case servomoveend:
+                    servo1.write(160);
+                    delay(15);
+                    servo2.write(140);
+                    delay(15);
+                    servo3.write(20);
+                    delay(15);
+                    servo4.write(100);
+                    delay(15);
+
+                    stateservo = servoopen;
+
+                    break;
+
+            }
+        case statebtn2:
+            digitalWrite(LEDPin_Blue, LOW);
+            digitalWrite(LEDPin_Red, HIGH);
+            digitalWrite(LEDPin_Green, LOW);
+
+            switch(stateservo){
+                case servomovestart:
+                    servo1.write(160);
+                    delay(15);
+                    servo2.write(140);
+                    delay(15);
+                    servo3.write(20);
+                    delay(15);
+                    servo4.write(100);
+                    delay(15);
+
+                    stateservo = servoopen;
+
+                    statesave = 1;
+
+                    break;
+                
+                case servoopen:
+                    servo5.write(180);
+                    delay(15);
+
+                    if(statesave == 1){
+                        statesave = 0;
+                        counter ++;
+                        if(counter >= 25){
+                            stateservo = servoclose;
+                            counter = 0;
+                        } 
+                    }
+                    else{
+                        statebtn = statebegin;
+                    }
+                    
+                    break;
+
+                case servoclose:
+                    servo5.write(0);
+                    if(SERVO5PIN >= 0){
+                        counter ++;
+                        if(counter >= 10){
+                            stateservo = servomoveend;
+                            counter = 0;
+                        }
+                    }
+
+                    break;
+
+                case servomove:
+                    servo2.write(90);
+                    delay(15);
+                    servo3.write(90);
+                    delay(15);
+                    servo4.write(90);
+                    delay(15);
+                    servo1.write(50);
+                    delay(15);
+                    
+                    stateservo = servomoveend;
+
+                    break;
+
+                case servomoveend:
+                    servo1.write(50);
+                    delay(15);
+                    servo2.write(140);
+                    delay(15);
+                    servo3.write(20);
+                    delay(15);
+                    servo4.write(100);
+                    delay(15);
+
+                    stateservo = servoopen;
+
+                    break;
+
             }   
+        
     } 
 }
+
